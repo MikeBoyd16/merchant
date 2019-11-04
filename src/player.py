d026b8bd67dd90130817bfa379a30b6c1f577032
MikeBoyd16 <mikeboyd165@gmail.com>
2019-11-04 13:52:34 -0800

    Add basic Player attributes

--- src/player.py
+++ src/player.py
@@ -4,5 +4,12 @@
 
 
 class Player:
-    def _init_(self):
-        return
+    def __init__(self, name, race, region):
+        self.name = name
+        self.race = race
+        self.region = region
+
+
+if __name__ == "__main__":
+    player1 = Player("Sam", "elf", "north")
+    print(player1.name + " is a/an " + player1.race + " from the " + player1.region + " region.")
