adab28d0f1f6afd1a21a44ef1c2a25fcc55cf416
MikeBoyd16 <mikeboyd165@gmail.com>
2019-11-04 18:32:02 -0800

    Add and integrate JSON file containing item data

--- src/item.py
+++ src/item.py
@@ -1,19 +1,22 @@
 """
 
 """
+import json
+with open('data/item_data.json') as data_file:
+    item_data = json.load(data_file)
 
 
 class Item:
-    def __init__(self, item_id, name, item_type, base_value, description):
+    def __init__(self, item_id):
         self.id = item_id
-        self.name = name
-        self.type = item_type
-        self.base_value = base_value
-        self.description = description
+        self.name = item_data[item_id]["name"]
+        self.type = item_data[item_id]["type"]
+        self.base_value = item_data[item_id]["base_value"]
+        self.description = item_data[item_id]["description"]
         self.quantity = 0
 
 
 if __name__ == "__main__":
-    item1 = Item(1, "Health Potion", "Consumable", 15, "Restores 25 HP")
+    item1 = Item("1")
     print(item1.name + " is a " + item1.type + " that costs " +
           str(item1.base_value) + " gold and " + item1.description)
--- src/player.py
+++ src/player.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     player1 = Player("Sam", "elf", "north")
     print(player1.name + " is a/an " + player1.race + " from the " + player1.region + " region.")
 
-    item1 = Item(1, "Health Potion", "Consumable", 15, "Restores 25 HP")
+    item1 = Item("1")
     player1.inventory[item1.id] = item1
     print(player1.inventory[item1.id].name + " is a " + player1.inventory[item1.id].type + " that costs " +
           str(player1.inventory[item1.id].base_value) + " gold and " + player1.inventory[item1.id].description)
