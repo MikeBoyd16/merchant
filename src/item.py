9f9168b6108355b215ce8cb8d10c73d2d32f6f2a
MikeBoyd16 <mikeboyd165@gmail.com>
2019-11-04 16:38:17 -0800

    Add inventory attribute to Player and rework Item attributes

--- src/item.py
+++ src/item.py
@@ -4,14 +4,16 @@
 
 
 class Item:
-    def __init__(self, name, item_type, value, description):
+    def __init__(self, item_id, name, item_type, base_value, description):
+        self.id = item_id
         self.name = name
-        self.item_type = item_type
-        self.value = value
+        self.type = item_type
+        self.base_value = base_value
         self.description = description
+        self.quantity = 0
 
 
 if __name__ == "__main__":
-    item1 = Item("Health Potion", "Consumable", 15, "Restores 25 HP")
-    print(item1.name + " is a " + item1.item_type + " that costs " +
-          str(item1.value) + " gold and " + item1.description)
+    item1 = Item(1, "Health Potion", "Consumable", 15, "Restores 25 HP")
+    print(item1.name + " is a " + item1.type + " that costs " +
+          str(item1.base_value) + " gold and " + item1.description)
--- src/player.py
+++ src/player.py
@@ -1,6 +1,7 @@
 """
 
 """
+from item import *
 
 
 class Player:
@@ -8,8 +9,14 @@ class Player:
         self.name = name
         self.race = race
         self.region = region
+        self.inventory = {}
 
 
 if __name__ == "__main__":
     player1 = Player("Sam", "elf", "north")
     print(player1.name + " is a/an " + player1.race + " from the " + player1.region + " region.")
+
+    item1 = Item(1, "Health Potion", "Consumable", 15, "Restores 25 HP")
+    player1.inventory[item1.id] = item1
+    print(player1.inventory[item1.id].name + " is a " + player1.inventory[item1.id].type + " that costs " +
+          str(player1.inventory[item1.id].base_value) + " gold and " + player1.inventory[item1.id].description)
