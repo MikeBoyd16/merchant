f26db962cecb8e6c6c5d2cf4d7f4d9140fddc9d2
MikeBoyd16 <mikeboyd165@gmail.com>
2019-11-13 12:10:13 -0800

    Create class Activity and add it as parent class to Shop

---
+++ src/activity.py
@@ -0,0 +1,14 @@
+"""
+The Activity class
+"""
+
+
+class Activity:
+    def __init__(self, activity_id, name, option_references):
+        self.id = activity_id
+        self.name = name
+        self.options = option_references
+
+
+if __name__ == "__main__":
+    pass
--- src/shop.py
+++ src/shop.py
@@ -1,29 +1,31 @@
 """
 The Shop class
 """
-from location import *
-with open('data/shop_data.json') as data_file:
-    shop_data = json.load(data_file)
+import json
+from activity import *
+with open('data/activity_data.json') as data_file:
+    activity_data = json.load(data_file)
 
 
-class Shop:
+class Shop(Activity):
     def __init__(self, shop_id):
-        self.id = shop_id
-        self.name = location_data[self.id]["name"]
-        self.owner = location_data[self.id]["owner"]
-        self.jobs = location_data[self.id]["jobs"]
-        self.inventory = location_data[self.id]["inventory"]
-        self.hours_of_operation = location_data[self.id]["hours_of_operation"]
-        self.current_npcs = location_data[self.id]["current_npcs"]
+        super().__init__(shop_id, activity_data["SHOPS"][shop_id]["name"],
+                         activity_data["SHOPS"][shop_id]["option_references"])
+        self.owner = activity_data["SHOPS"][self.id]["owner"]
+        self.jobs = activity_data["SHOPS"][self.id]["jobs"]
+        self.inventory = activity_data["SHOPS"][self.id]["inventory"]
+        self.hours_of_operation = activity_data["SHOPS"][self.id]["hours_of_operation"]
+        self.current_npcs = activity_data["SHOPS"][self.id]["current_npcs"]
 
 
 if __name__ == "__main__":
     """ SHOP CREATION """
-    shop1 = Shop("2")
-    assert shop1.id == "1"
-    assert shop1.name == shop_data[shop1.id]["name"]
-    assert shop1.owner == shop_data[shop1.id]["owner"]
-    assert shop1.jobs == shop_data[shop1.id]["jobs"]
-    assert shop1.inventory == shop_data[shop1.id]["inventory"]
-    assert shop1.hours_of_operation == shop_data[shop1.id]["hours_of_operation"]
-    assert shop1.current_npcs == shop_data[shop1.id]["npcs"]
+    shop1 = Shop("SHOP_001")
+    assert shop1.id == "SHOP_001"
+    assert shop1.name == activity_data["SHOPS"][shop1.id]["name"]
+    assert shop1.owner == activity_data["SHOPS"][shop1.id]["owner"]
+    assert shop1.jobs == activity_data["SHOPS"][shop1.id]["jobs"]
+    assert shop1.inventory == activity_data["SHOPS"][shop1.id]["inventory"]
+    assert shop1.hours_of_operation == activity_data["SHOPS"][shop1.id]["hours_of_operation"]
+    assert shop1.current_npcs == activity_data["SHOPS"][shop1.id]["current_npcs"]
+    assert shop1.options == activity_data["SHOPS"][shop1.id]["option_references"]
